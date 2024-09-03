3c92252b60b8beba2431ae6c1778766589dcbb13
Muhammad Aqib Mehmood <muhammadaqibmehmood73@gmail.com>
2024-09-03 17:26:12 +0500

    .created xcode project

---
+++ FAQSectionWithToggle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FAQSectionWithToggle
+//
+//  Created by Mapple.pk on 03/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
